e3c822cee54bbb147f4d82418b9d2aa0a254b893
Laszlo Pokoradi <laszlo.pokoradi@gmail.com>
2025-05-10 13:38:07 +0200

    Use INSERT IGNORE in data.sql to prevent duplicate entries

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO activities (id, name)
+INSERT IGNORE INTO activities (id, name)
 VALUES (1, 'Petting'),
        (2, 'Walking'),
        (3, 'Feeding'),
        (4, 'Medicating'),
        (5, 'Shaving');
 
-INSERT INTO pet_types (id, name)
+INSERT IGNORE INTO pet_types (id, name)
 VALUES (1, 'CAT'),
        (2, 'DOG'),
        (3, 'LIZARD'),
@@ -14,7 +14,7 @@ VALUES (1, 'CAT'),
        (6, 'SNAKE'),
        (7, 'OTHER');
 
-INSERT INTO pet_type_activities (pet_type_id, activity_id)
+INSERT IGNORE INTO pet_type_activities (pet_type_id, activity_id)
 VALUES (1, 3),
        (1, 4),
        (2, 1);
